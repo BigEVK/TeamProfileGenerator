a89015ad31e31393f8a7317a6f62a685bde53012
Eric VanKirk <eric@ericvankirk.com>
2022-08-03 18:08:23 -0500

    initial commit

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,21 @@
+module.xportes = {
+    name: 'Manager Person',
+    employeeID: 'MP01',
+    email: 'MP01@work.com',
+    phone: '713-806-7197'
+    confirmAddTeam: true, 
+}, 
+{
+    name: 'Manager Persons',
+    employeeID: 'MP02',
+    email: 'MP02@work.com',
+    phone: '713-806-7197'
+    confirmAddTeam: true,
+},
+{
+    name: 'Manager People',
+    employeeID: 'MP03',
+    email: 'MP03@work.com',
+    phone: '713-806-7197'
+    confirmAddTeam: true,
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+
+const inquirer = require('inquirer');
+const generatePage = require('/src/pageTemplate');
+const { writeFile, copyFile } = require('./utils/generateSite')
+
+
+
+
+
+const promptTeam = () => {
+    console.log(`
+    ==================
+    Add a Team Memeber
+    ==================
+    `)
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'team',
+            message: 'Please choose which Team Member you would like to add. (Required)',
+            choices: ["Manager", "Engineer", "Intern", "Finished"],
+            validate: teamInput => {
+                if (teamInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a Team Member to add or select Finished.');
+                }
+            }
+        }
+    ])
+}
